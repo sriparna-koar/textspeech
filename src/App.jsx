24243902e0ec519ff4dd9f5208fa9d9f099cb8c0
sriparna-koar <koarsk03@gmail.com>
2024-05-12 14:26:55 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,4 @@
 
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import TextToSpeech from './TextToSpeech';
-// import NavigationBar from './components/NavigationBar';
-// import FileSummarizer from './FileSummarizer';
-// function App() {
-//   return (
-//     <Router basename={import.meta.env.BASE_URL}>
-//       <div>
-//       <NavigationBar />
-//       <Routes>
-//       <Route path="/" element={<FileSummarizer />} />
-//       <Route path="/text-to-speech" element={<TextToSpeech />} />
-
-//       </Routes>
-    
-    
-//       </div>
-//     </Router>
-    
-//   )
-//       };
-
-// export default App;
 import React from 'react';
 import TextToSpeech from './TextToSpeech';
 import FileSummarizer from './FileSummarizer';
